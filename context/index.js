ad7c44ec40f7a985ae62f8ff6969999c9d5b0f27
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-03-22 15:08:20 -0400

    Fixture loaded from disk

--- context/index.js
+++ context/index.js
@@ -15,19 +15,7 @@
   });
 
   require(['heatmap_scatterplot', 'd3'], function (heatmap_scatterplot, d3) {
-    // TODO: read data from disk
-    var matrix = [['gene', 'cond-a', 'cond-b', 'cond-c', 'cond-d']];
-    for (var i = 0; i < 40000; ++i) {
-      var a = Math.asin((Math.random() - 0.5) * 2);
-      var b = Math.asin((Math.random() - 0.5) * 2);
-      var c = 0.5 * a + b + 0.1 * (Math.random() - 0.5);
-      var d = a + 0.5 * b + 0.1 * (Math.random() - 0.5);
-      matrix.push(["gene-" + i, a, b, c, d]);
-    }
-    var data_uri = "data:text/tab-separated-values," + encodeURIComponent(
-      matrix.map(function (row) {
-        return row.join("\t")
-    }).join("\n"));
+    var data_uri = "data/fake-data.tsv";
     d3.tsv(data_uri, function (error, matrix) {
       if (error) throw error;
       d3.select('#container')
